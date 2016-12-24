b5206a9a1a57c9b4a981459d20bb3e1bc3bf057a
anchen <you@example.com>
2016-12-24 19:39:18 +1100

    add cinema information on movie details response

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -12,7 +12,7 @@ export const cinemas = [
 export const moviesListEndpoint = '/movies';
 export const movieDetailEndpoint = '/movie/';
 export const accessToken = 'sjd1HfkjU83ksdsm3802k';
-export const requestDelay = 50;// retry request delay 50ms
-export const requestTimeOut = 300;// proxy request time out 300ms
-export const globalTimeout = 15000;// total response timeout 15s
+export const requestDelay = 500;// retry request delay 500ms
+export const requestTimeOut = 1000;// proxy request time out 1s
+export const globalTimeout = 30000;// total response timeout 30s
 
