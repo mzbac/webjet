a88bc5f23bdefa7ca18aa9ba1fcd71c1b4945732
anchen <you@example.com>
2016-12-26 10:29:58 +1100

    refactoring project structrue

---
+++ src/client/components/MovieItem.js
@@ -0,0 +1,79 @@
+import React, { Component, PropTypes } from 'react';
+import { Item, Rating } from 'semantic-ui-react';
+import _ from 'lodash';
+import {
+  movieItemMaxRating,
+  movieItemDefaultRatingDivider,
+  defaultPosterUrl,
+} from '../config';
+
+
+class MovieItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      postImage: defaultPosterUrl,
+    };
+  }
+
+  componentDidMount() {
+    const { movie: { Poster } } = this.props;
+    this.checkPosterURL(Poster);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { movie: { Poster: newPoster } } = nextProps;
+    const { movie: { Poster: currPoster } } = this.props;
+    if (newPoster === currPoster) return;
+    this.checkPosterURL(newPoster);
+  }
+
+  checkPosterURL(url) {
+    const img = new Image();
+    img.onload = () => {
+      this.setState({ postImage: url });
+    };
+    img.src = url;
+  }
+
+  render() {
+    const { movie } = this.props;
+    const { postImage } = this.state;
+    const {
+      Plot,
+      Title,
+      cinema,
+      Price,
+      Rating: rating,
+    } = movie;
+    return (
+      <Item>
+        <Item.Image size="small" src={postImage} />
+        <Item.Content>
+          <Item.Header>{Title}</Item.Header>
+          <Item.Description>
+            <h3>
+              Cinema: {cinema.name}
+            </h3>
+            <h3>
+              Price: ${Price}
+            </h3>
+            <p>{Plot}</p>
+            <Rating
+              icon="star"
+              disabled
+              defaultRating={_.toNumber(rating) / movieItemDefaultRatingDivider}
+              maxRating={movieItemMaxRating}
+            />
+          </Item.Description>
+        </Item.Content>
+      </Item>
+    );
+  }
+}
+
+MovieItem.propTypes = {
+  movie: PropTypes.object,
+};
+export default MovieItem;
--- src/client/config/index.js
+++ src/client/config/index.js
@@ -4,3 +4,4 @@ export const searchMoviesThrottleTime = 100;
 export const movieIDPrefixIndex = 2;
 export const movieItemMaxRating = 5;
 export const movieItemDefaultRatingDivider = 2;
+export const defaultPosterUrl = 'https://firebasestorage.googleapis.com/v0/b/sizzling-torch-9797.appspot.com/o/image.png?alt=media&token=3893e55c-e3a2-4937-aaef-5b777a87cd33';
